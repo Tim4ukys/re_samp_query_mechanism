ad8eedaac8bb6cb2d71e30112cc7e21fcd1e6489
Tim4ukys <73384372+Tim4ukys@users.noreply.github.com>
2022-04-24 13:23:11 +0700

    add example.cpp

---
+++ example.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <boost/asio.hpp>
+#include <cstring>
+#include <sstream>
+#include <memory.h>
+#include <random>
+
+#include "SQM.h"
+
+int randomInteger(int min, int max)
+{
+	static std::random_device rd;
+	static std::default_random_engine e1(rd());
+	std::uniform_int_distribution<int> uniform_dist(min, max);
+	return uniform_dist(e1);
+}
+
+int main(int argc, char* argv[])
+{
+	SQM sm{"01.gtarp.ru", "7777"};
+
+	std::cout << "----- INFO SERVER -----" << std::endl;
+	sm.sendRequest(SQM::eRequestType::SERVERINFO);
+	auto inf = sm.receive<SQM::stServerInfo>();
+	std::cout << "Host name: " << inf.m_sHostName << std::endl
+		<< "Player: " << inf.m_nPlayers << "/" << inf.m_nMaxPlayers << std::endl;
+
+	std::cout << "----- RULES -----" << std::endl;
+	sm.sendRequest(SQM::eRequestType::SERVERRULES);
+	auto rul = sm.receive<SQM::stServerRules>();
+	for (const auto& [rkey, rr] : rul.m_arrRules) {
+		std::cout << rkey.c_str() << ": " << rr.c_str() << std::endl;
+	}
+
+	std::cout << "----- PLAYERS -----" << std::endl;
+	sm.sendRequest(SQM::eRequestType::PLAYERLIST);
+	auto players = sm.receive<SQM::stPlayerList>();
+	size_t i_player{};
+	for (const auto& pl : players.m_arrPlayer) {
+		std::cout << "Nick: " << pl.m_sName << "; Score: " << pl.m_nScore << std::endl;
+		if (++i_player > 5) {
+			std::cout << "..." << std::endl;
+			break;
+		}
+	}
+
+	std::cout << "----- PING -----" << std::endl;
+	int rand = randomInteger(INT_MIN, INT_MAX);
+	UINT64 tick = GetTickCount64();
+	sm.sendRequest(SQM::eRequestType::SERVERPING, &rand, 4);
+	sm.receive<SQM::stServerPing>();
+	tick = GetTickCount64() - tick;
+	std::cout << "Ping: " << (int)tick << std::endl;
+
+
+
+	return EXIT_SUCCESS;
+}
